283b49a3634fb5d496053370051017b4cddc4195
Afham Siddiqui <62937387+Afham-Siddiqui@users.noreply.github.com>
2020-04-09 15:37:03 +0500

    Add files via upload

---
+++ even odd checker.py
@@ -0,0 +1,7 @@
+print("EVEN OR ODD NUMBERS CHECKER")
+a = int(input("enter your number..."))
+if (a % 2) == 0:
+    print("the number you entered is even")
+
+else:
+    print("the number you entered is odd")
---
+++ password checker.py
@@ -0,0 +1,10 @@
+n = str(input("enter first name.."))
+a = str(input("enter last name.."))
+m = (input("enter your date of birth.."))
+e = (input("enter your age.."))
+s = (input("enter your password.."))
+if s == ("0123456789"):
+    print("access denied")
+
+else:
+    print("wrong password")
